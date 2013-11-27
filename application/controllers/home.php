6ebb6f2c35c0d8e0fdb0ce1aa6ac894921156a23
Sumit Jain <sumeetsuniljain@gmail.com>
2013-11-27 11:08:35 +0530

    3

---
+++ application/controllers/home.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('home');
+	}
+}
+
+/* End of file accounts.php */
+/* Location: ./application/controllers/accounts.php */
---
+++ application/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Novus Software Services | Home
+	</title>
+</head>
+<body>
+	
+</body>
+</html>
