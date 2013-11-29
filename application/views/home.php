91891e27a9957ce219c69611d260b652d64154b3
Jitendra Kachhawa <jitendra.kachhawa07@gmail.com>
2013-11-29 14:44:58 +0530

    7

--- application/views/home.php
+++ application/views/home.php
@@ -6,6 +6,54 @@
 	</title>
 </head>
 <body>
-	
+
+<div id= "dropdown_nav">
+<div id= "nav_headers">
+<span class= "home">Home</span>
+<span class= "company">Company</span>
+<span class= "services">Services</span>
+<span class= "products">Products</span>
+</div>
+
+<div id= "nav_menus">
+<ul class= "home">
+</ul>
+
+<ul class= "company">
+<li><a href= "#">Vision</a></li>
+<li><a href= "#">Mission</a></li>
+<li><a href= "#">Management</a></li>
+<li><a href= "#">Values</a></li>
+<li><a href= "#">Contacts</a></li>
+<li><a href= "#">Support</a></li>
+<li><a href= "#">Careers</a></li>
+</ul>
+
+<ul class= "services">
+<li><a href= "#">mobile App development</a></li>
+<li><a href= "#">Web Design & Development</a></li>
+<li><a href= "#">Networking Services</a></li>
+<li><a href= "#">Business IT Services</a></li>
+<li><a href= "#">Retail IT Solutions</a></li>
+<li><a href= "#">Medical IT Solutions</a></li>
+<li><a href= "#">Manufacturing IT Solutions</a></li>
+<li><a href= "#">Education Information Systems</a></li>
+<li><a href= "#">Online marketing</a></li>
+<li><a href= "#">Customer Relationship Management</a></li>
+<li><a href= "#">Data warehousing</a></li>
+<li><a href= "#">Google Apps For Business Setup</a></li>
+<li><a href= "#">MS Office 365 Setup & Maintenance</a></li>
+<li><a href= "#">Enterprise Resource planning</a></li>
+<li><a href= "#">Product Life Cycle Management</a></li>
+<li><a href= "#">Supply Chain Management</a></li>
+<li><a href= "#">Business Analysis Services</a></li>
+</ul>
+
+<ul class= "products">
+<li><a href= "#">Acharya</a></li>
+<li><a href= "#">AcharyaCA</a></li>
+</ul>
+</div>
+</div>
 </body>
 </html>
